b9ba3904ee66d86bce29327cacd1da7b2a1cddbd
germanlion67 <143081644+germanlion67@users.noreply.github.com>
2025-09-04 14:02:43 +0200

    Update nextcloud_sync_service.dart

--- lib/services/nextcloud_sync_service.dart
+++ lib/services/nextcloud_sync_service.dart
@@ -4,54 +4,80 @@ import 'package:webdav_client/webdav_client.dart' as webdav;
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:logger/logger.dart';
+import 'nextcloud_credentials.dart';
 
 class NextcloudSyncService {
-  final String serverUrl = 'https://deine-nextcloud-url.de/remote.php/dav/files/USERNAME/';
-  final String username = 'USERNAME';
-  final String password = 'PASSWORT';
-  final String remoteFolder = 'ArtikelSync';
-
   late webdav.Client client;
   final Logger _logger = Logger();
+  late String remoteFolder;
 
-  NextcloudSyncService() {
-    client = webdav.newClient(
-      serverUrl,
-      user: username,
-      password: password,
-    );
-  }
-
-  Future<void> uploadJsonFile(String fileName) async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/$fileName');
-
-    if (await file.exists()) {
-      await client.write('$remoteFolder/$fileName', file.readAsBytesSync());
-      _logger.i('Datei erfolgreich hochgeladen: $fileName');
-    } else {
-      _logger.w('Datei nicht gefunden: $fileName');
+  Future<bool> init() async {
+    try {
+      final creds = await NextcloudCredentialsStore().read();
+      if (creds == null) {
+        _logger.e('Nextcloud-Zugangsdaten nicht gefunden.');
+        return false;
+      }
+      client = webdav.newClient(
+        creds.server.toString(),
+        user: creds.user,
+        password: creds.appPw,
+      );
+      remoteFolder = creds.baseFolder;
+      return true;
+    } catch (e) {
+      _logger.e('Fehler bei Initialisierung der Nextcloud-Verbindung:', e);
+      return false;
     }
   }
 
-  Future<void> downloadJsonFile(String fileName) async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = File('${dir.path}/$fileName');
-
-    final data = await client.read('$remoteFolder/$fileName');
-    await file.writeAsBytes(data);
-    _logger.i('Datei erfolgreich heruntergeladen: $fileName');
+  Future<bool> uploadJsonFile(String fileName) async {
+    try {
+      final dir = await getApplicationDocumentsDirectory();
+      final file = File('${dir.path}/$fileName');
+      if (await file.exists()) {
+        await client.write('$remoteFolder/$fileName', file.readAsBytesSync());
+        _logger.i('Datei erfolgreich hochgeladen: $fileName');
+        return true;
+      } else {
+        _logger.w('Datei nicht gefunden: $fileName');
+        return false;
+      }
+    } catch (e) {
+      _logger.e('Fehler beim Hochladen der Datei:', e);
+      return false;
+    }
   }
 
-  Future<void> uploadImage(String imagePath) async {
-    final imageFile = File(imagePath);
-    final imageName = imageFile.uri.pathSegments.last;
+  Future<bool> downloadJsonFile(String fileName) async {
+    try {
+      final dir = await getApplicationDocumentsDirectory();
+      final file = File('${dir.path}/$fileName');
+      final data = await client.read('$remoteFolder/$fileName');
+      await file.writeAsBytes(data);
+      _logger.i('Datei erfolgreich heruntergeladen: $fileName');
+      return true;
+    } catch (e) {
+      _logger.e('Fehler beim Herunterladen der Datei:', e);
+      return false;
+    }
+  }
 
-    if (await imageFile.exists()) {
-      await client.write('$remoteFolder/images/$imageName', imageFile.readAsBytesSync());
-      _logger.i('Bild erfolgreich hochgeladen: $imageName');
-    } else {
-      _logger.w('Bild nicht gefunden: $imagePath');
+  Future<bool> uploadImage(String imagePath) async {
+    try {
+      final imageFile = File(imagePath);
+      final imageName = imageFile.uri.pathSegments.last;
+      if (await imageFile.exists()) {
+        await client.write('$remoteFolder/images/$imageName', imageFile.readAsBytesSync());
+        _logger.i('Bild erfolgreich hochgeladen: $imageName');
+        return true;
+      } else {
+        _logger.w('Bild nicht gefunden: $imagePath');
+        return false;
+      }
+    } catch (e) {
+      _logger.e('Fehler beim Hochladen des Bildes:', e);
+      return false;
     }
   }
 }
