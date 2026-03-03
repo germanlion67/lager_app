c0887e7de36578faebb4068958a71cbb76cf2847
germanlion67 <143081644+germanlion67@users.noreply.github.com>
2026-03-03 12:41:38 +0100

    Create database_service.dart

---
+++ app/lib/services/database_service.dart
@@ -0,0 +1,11 @@
+import 'package:pocketbase/pocketbase.dart';
+import 'package:flutter/foundation.dart';
+
+class DatabaseService {
+  // Ersetze 192.168.178.XX mit der IP deines Rechners im WLAN
+  static final String baseUrl = kIsWeb 
+      ? 'http://localhost:8090' 
+      : 'http://192.168.178.XX:8090'; 
+
+  static final pb = PocketBase(baseUrl);
+}
